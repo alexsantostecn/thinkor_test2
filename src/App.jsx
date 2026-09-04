654944184880020c1bfa52f85906d8972a3a117b
Álex Santos <alexsantostecn@gmail.com>
2026-09-04 14:55:11 -0300

    Update título do React

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Meu primeiro react :)</h1>
+          <h1>Meu segundo react :)</h1>
           <p>
             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
           </p>
